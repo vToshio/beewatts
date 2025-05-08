40b7dd28523040fe262e3fbc4979d146c81c6a11
vtoshio <vtoshio2004@gmail.com>
2025-05-08 15:22:10 -0300

    feat: adicionar a endpoint e estilo da página de cadastro de endereço

--- simulador/urls.py
+++ simulador/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from simulador.views import index
+from simulador.views import index, endereco
 
 urlpatterns = [
-    path('', index, name='iniciar_calculo')
+    path('', index, name='iniciar_calculo'),
+    path('endereco/', endereco, name='cadastrar_endereco')
 ]
--- simulador/views.py
+++ simulador/views.py
@@ -4,3 +4,6 @@ from django.http import HttpRequest
 # Create your views here.
 def index(request: HttpRequest):
     return render(request, 'simulador/index.html')
+
+def endereco(request: HttpRequest):
+    return render(request, 'simulador/endereco.html')
