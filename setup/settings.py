0f53b961217f5e0b598c874a57c6fa36267d5e1e
vtoshio <vtoshio2004@gmail.com>
2025-06-03 00:28:52 -0300

    deploy: configuração dos hosts permitidos

--- setup/settings.py
+++ setup/settings.py
@@ -27,10 +27,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG'))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'beewatts.com.br'
+]
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+    'localhost:8000',
+]
+
+CSRF_TRUSTED_ORIGIN = [
+    'beewatts.com.br'
+]
 
 # Application definition
 
